539a235d2c7c52d50ca6853266409617df683602
stanley chukwu <stanleychukwu17@yahoo.com>
2022-09-24 11:54:26 +0100

    added a function for getting the next sibling added a lerp function for getting linear interpolation calculation

--- src/functions/utils.ts
+++ src/functions/utils.ts
@@ -1,6 +1,9 @@
 // Linear interpolation
+const lerP = (a: number, b: number, n: number): number => {
+    return (1-n) * a+n * b;
+}
 
-
+// Gets the mouse position
 const getMousePosition = (e:MouseEvent): {x: number, y:number} => {
     let posX = 0
     let posY = 0
@@ -16,6 +19,29 @@ const getMousePosition = (e:MouseEvent): {x: number, y:number} => {
 };
 
 
+// Get siblings
+const getSiblings = (e:HTMLElement) => {
+    // for collecting siblings
+    let siblings: HTMLElement|ChildNode[] = []
+
+    // if no parent, return no sibling
+    if (!e.parentNode) {
+        return siblings
+    }
+
+    // first child of the parent node
+    let sibling = e.parentNode.firstChild
+    //collecting siblings
+    while (sibling) {
+        if (sibling.nodeType === 1 && sibling !== e) {
+            siblings.push(sibling)
+        }
+
+        sibling = sibling.nextSibling
+    }
+
+    return siblings
+}
 
 
-export {getMousePosition}
+export {getMousePosition, lerP, getSiblings }
