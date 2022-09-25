c695af9770a166d8312d46283b65e119ea914421
stanley chukwu <stanleychukwu17@yahoo.com>
2022-09-25 20:29:23 +0100

    still on the cursor but we now added the jquery package

--- src/functions/utils.ts
+++ src/functions/utils.ts
@@ -1,3 +1,5 @@
+import {gsap, Power3} from "gsap";
+
 // Linear interpolation
 const lerP = (a: number, b: number, n: number): number => {
     return (1-n) * a+n * b;
@@ -48,30 +50,30 @@ class Cursor {
     public cursor:HTMLDivElement;
     public item:NodeListOf<HTMLDivElement>;
     public cursorConfigs = {
-        x: {previous: 0, current: 0, amt:.2},
-        y: {previous: 0, current: 0, amt:.2}
+        x: {previous: 0, current: 0, amt:.1},
+        y: {previous: 0, current: 0, amt:.1}
     }
     public onMouseMoveEv: Function;
+    public mouse: {x:number, y:number}
 
     constructor(el: HTMLDivElement, mouse: {x: number, y: number}) {
         this.cursor = el
         this.cursor.style.opacity = '0'
         this.item = document.querySelectorAll('.Bsk-Ech')
+        this.mouse = mouse
 
         // updates the cursor position every time the mouse moves
         this.onMouseMoveEv = () => {
+            this.mouse = mouse
             this.cursorConfigs.x.previous = this.cursorConfigs.x.current = mouse.x
             this.cursorConfigs.y.previous = this.cursorConfigs.y.current = mouse.y
-            this.cursor.style.opacity = '1'
+            gsap.to(this.cursor, {opacity: '1', duration: 1, ease: Power3.easeOut})
 
             /** 
                 calls the windows.requestAnimationFrame() method, this method tells the browser that you wish to perform an animation and makes sure the browser calls a specified function to update an animation
                 before the next browser repaint. the method takes a callback as an argument to be executed before the repaint is done.
-                // requestAnimationFrame(this.Render(mouse))
             */
-
-            // instead of using the window.requestAnimationFrame() method, i decided to just call the this.Render method instead.
-            this.Render(mouse)
+            requestAnimationFrame(() => this.Render())
 
             //@ts-ignore
             window.removeEventListener('mousemove', this.onMouseMoveEv)
@@ -83,10 +85,40 @@ class Cursor {
         window.addEventListener("mousemove", this.onMouseMoveEv);
     }
 
-    Render (mouse: {x: number, y: number}) {
-        this.cursorConfigs.x.previous = mouse.x
-        this.cursorConfigs.y.previous = mouse.y
-        console.log('somebody calling me')
+    Render () {
+        this.cursorConfigs.x.previous = this.mouse.x
+        this.cursorConfigs.y.previous = this.mouse.y
+
+        /**
+            i feel like he over engineered the problem below, 
+            we could simply have done
+            let x = this.cursorConfigs.x
+            let y = this.cursorConfigs.y
+
+            this.cursorConfigs.x.previous = lerP(x.previous, x.current, x.amt)
+            this.cursorConfigs.y.previous = lerP(y.previous, y.current, y.amt)
+
+            as simple as the above and readable as well
+        */
+        for (const key in this.cursorConfigs) {
+            //WTF IS LER.P?
+            // Ler.p - A ler returns the value between two numbers at a specified, decimal midpoint
+            // please check the video for more links regarding this lerP function
+
+            //@ts-ignore
+            this.cursorConfigs[key].previous = lerP(this.cursorConfigs[key].previous, this.cursorConfigs[key].current, this.cursorConfigs[key].amt)
+        }
+
+        // this.cursor.style.transform = `translateX(${this.cursorConfigs.x.previous}px)
+        //     translateY(${this.cursorConfigs.y.previous}px)`
+        gsap.to(this.cursor, {
+            x: `${this.cursorConfigs.x.previous}px`,
+            y:`${this.cursorConfigs.y.previous}px`,
+            duration: 0.01,
+            ease: Power3.easeOut
+        })
+
+        requestAnimationFrame(() => this.Render())
     }
 }
 
