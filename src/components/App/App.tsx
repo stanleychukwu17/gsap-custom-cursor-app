7c551d4c98f0d9335aac56eb8dde3f0af8397b18
stanley chukwu <stanleychukwu17@yahoo.com>
2022-09-28 10:31:38 +0100

    Improved the comments on the App.tsx file, want the code to be readable to everyone

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,30 +1,29 @@
 import { useEffect, useState, useRef } from 'react';
 import './app.scss';
-// import { motion } from 'framer-motion';
-// import { gsap } from 'gsap';
 
+//getMousePosition: gets the position of the mouse anytime the user moves their mouse
+//Cursor: this is the class object that handles everything with how the custom cursor reacts to different elements on the page
 import { getMousePosition, Cursor } from '../../functions/utils';
-
 
 // import the video files
 const website = require('../../assets/video/website.mp4')
 const app = require('../../assets/video/app.mp4')
 const brand = require('../../assets/video/brand.mp4')
 
-
 // types
 type mouseType = {x:number, y:number}
 
 const App = () => {
-    const [mouse, setMouse] = useState<mouseType>({} as mouseType)
+    const [mouse, setMouse] = useState<mouseType>({} as mouseType) // stores the co-ordinates of the mouse position
     // const [cursor, setCursor] = useState<Cursor>({} as Cursor)
-    const cursor = useRef<Cursor>({} as Cursor)
+    const cursor = useRef<Cursor>({} as Cursor) // Inside the useEffect, this.current will be an instanceof cursor
 
     useEffect(() => {
+        // you know everyTime the mouse position changes, this useEffect function is called. so we do not want to create a new instance everyTime, instead, we update the mouse values
         if (cursor.current instanceof Cursor) {
             cursor.current.updateMouse(mouse)
         } else {
-            cursor.current = new Cursor(document.querySelector('.cursor')!, mouse)
+            cursor.current = new Cursor(document.querySelector('.cursor')!, mouse) // passes the .cursor, class to be used as our custom cursor
         }
     }, [mouse])
 
@@ -32,12 +31,12 @@ const App = () => {
         // updates the mouse position
         const trackMouse = (ev:MouseEvent) => setMouse(getMousePosition(ev))
 
-        // adds an event listener that allows us to know the current position of the user mouse.. if this was a production app, i would have removed the event when the user left the page
+        // adds an event listener that allows us to know the current position of the user mouse..
         window.addEventListener("mousemove", trackMouse)
 
         return function cleanup() {
             window.removeEventListener("mouseenter", trackMouse)
-            cursor.current.killAllEventListeners()
+            cursor.current.killAllEventListeners() // this .killAllEventListeners() method, kills all the event listeners added to the window object
         };
     }, [])
     
